7f0f99d93a9aaca876d6411391e7907bd0031159
Dave <dw@pop-os.localdomain>
2023-04-26 18:03:00 +0100

    my changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import "nes.css/css/nes.min.css";
 import './App.css';
+import Home from './components/Home';
+
+export enum AppState {
+  Home,
+  NewHabit,
+  MainPage
+}
 
 function App() {
+
+  const renderActiveComponent = () =>
+  {
+    switch (activeComponent) {
+      case AppState.Home:
+        return <Home></Home> 
+      default:
+        return <Home></Home> 
+    }
+  }
+
+  const [activeComponent, setActiveComponent] = useState(AppState.Home);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {renderActiveComponent()}
     </div>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+return (
+    <>
+      <div className="title-text">
+       DWINDLE 
+      </div>
+      <div className="info-text nes-text is-primary">
+        Habit Breaker 
+      </div>
+      <div className="main-menu-button-tray">
+        <button type="button" className="menu-button nes-btn is-success">Start</button>
+      </div>
+    </>);
+}
+
+export default Home
